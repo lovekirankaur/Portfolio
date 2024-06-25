9c66847ac642f667e9c617b2baa9f180f95f8fc7
lovekiran kaur <lovekiranbains@gmail.com>
2024-06-25 21:43:14 +0530

    Few Changes to Intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -90,8 +90,8 @@ export default function Intro() {
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
           focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
          
-          href=" "
-          download
+          href="lovekiranCV.pdf"
+          target="_blank"
         >
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
